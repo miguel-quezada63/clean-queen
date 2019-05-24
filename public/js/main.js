d3367e83a4b26013853a212be9f0bccb9d8a66b6
RawRedOnion <rawredonion@localhost.localdomain>
2019-05-24 14:17:14 -0400

    Fedora computer first commit

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+window.onscroll = () => {
+    const nav = document.querySelector("#navigation");
+    const navItem = document.querySelectorAll(".navbar-item");
+    if (this.scrollY <= 500) {
+        nav.classList.remove("navbar--color");
+        nav.classList.add("navbar--transparent");
+        for (let i = 0; i < navItem.length; i++) {
+            console.log(i);
+            navItem[i].classList.remove("navbar--black");
+            navItem[i].classList.add("navbar--white");
+        }
+    }
+    else {
+        nav.classList.add("navbar--color");
+        nav.classList.remove("navbar--transparent");
+        for (let i = 0; i < navItem.length; i++) {
+            console.log(i);
+            navItem[i].classList.remove("navbar--white");
+            navItem[i].classList.add("navbar--black");
+        }
+    }
+};
